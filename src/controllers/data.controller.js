b28da3db3b7a29f8c689e3d5ca8f81c619578b42
Fekete-Bruno <brunohffekete@gmail.com>
2022-09-10 18:23:42 -0300

    refactor: changed from 'data' to 'logs' collection

--- src/controllers/data.controller.js
+++ src/controllers/data.controller.js
@@ -11,9 +11,13 @@ async function findList(req,res){
     try {
         const session = await db.collection('sessions').findOne({token});
         if(!session) return res.sendStatus(401);
+
         const user = await db.collection('users').findOne({_id:session.userId});
         if(!user) return res.sendStatus(404);
+
+        const logs = await db.collection('logs').find({userId:session.userId}).toArray();
         delete user.password;
+        user.logs=logs;
         return res.send(user);
     } catch (error) {
         console.error(error);
